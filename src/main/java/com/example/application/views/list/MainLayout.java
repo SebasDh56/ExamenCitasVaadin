b5ffa52e04785c72503b45c0bad5c317edf895ef
SebasDh56 <amable.quishpe@epn.edu.ec>
2023-06-08 19:23:07 -0500

    first commit

---
+++ src/main/java/com/example/application/views/list/MainLayout.java
@@ -0,0 +1,8 @@
+package com.example.application;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.RouterLayout;
+
+public class MainLayout extends VerticalLayout implements RouterLayout {
+    // Puedes personalizar el contenido y la estructura del layout principal aquí
+}
